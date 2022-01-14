ed95a8564c046bc5d55de854492289187fff77a9
khaled-11 <e.khaled.abouseada@gmail.com>
2022-01-14 16:06:46 -0500

    Clean code

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 
+// Inline React style
 const style = {
   center:{
     display: 'flex', 
@@ -17,12 +18,8 @@ const style = {
     border: "0.7px solid black",
     textAlign:"center",
   },
-  formSectionHidden:{
-    display:"none"
-  },
   table:{
     borderCollapse: "collapse",
-    display:"full",
   },
   tableCell:{
     border: "0.7px solid black",
@@ -31,7 +28,9 @@ const style = {
   },
 }
 
+// The entire App is in one function component
 function App() {
+  // The default data for add contact form
   const initialContact = {
     first_name: "First Name",
     last_name: "Last Name",
@@ -40,11 +39,13 @@ function App() {
     detail: "N/A"
   };
 
+  // Use state to save contacts data and control the display
   const [contacts, setContacts] = React.useState([]);  
   const [showAdd, setShowAdd] = React.useState(false);
   const [contactData, setContactData] = React.useState(initialContact);
   const [error, setError] = React.useState("");  
 
+  // Load the contacts from the database
   React.useEffect(() => {
     fetch("http://localhost:3370/read_contacts",
     {
@@ -62,6 +63,7 @@ function App() {
       )
   }, [])
 
+  // Function to set the state of the form inputs
   const setFormData = (e) => {
     setError("")
     setContactData({
@@ -70,19 +72,24 @@ function App() {
     });
   };
 
+  // Function to handle the form submit
   const handleSubmit = (e) => {
+    // Prevent default and check for errors.
     e.preventDefault();
     let cuError = false
     for (let i = 0 ; i < contacts.length ; i++){
       if (contacts[i].first_name === contactData.first_name && contacts[i].last_name === contactData.last_name){
+        // Display Error
+        setError("This contact already exists.")
         cuError = true
       }
     }
     if (!contactData.first_name || !contactData.last_name || !contactData.phone || !contactData.email) {
+      setError("Some Fields can't be empty.")
       cuError = true
     }
+    // Return to prevent submit and call the API if not.
     if (cuError){
-      setError("This contact already exists.")
       return
     } else {
       fetch("http://localhost:3370/add_contact",
@@ -107,8 +114,8 @@ function App() {
     }
   };
 
+  // Delete Contact funtion
   const delFun = (index) => {
-    // Will call server and delete from database
     fetch("http://localhost:3370/delete_contact",
     {
        method: 'POST',
@@ -125,13 +132,16 @@ function App() {
         // }
       )
   };
+
+  // Edit contact function
   const editFun = (index) => {
     // Will display an edit form
-    // console.log(index)
+    console.log(index)
   };
 
+  // Display the contacts in the table function
   function Display () {
-    return contacts.length > 0 ? (
+    return (
       contacts.map((user, index) => (
         <tr key={index}>
           <td style={style.tableCell}>{user.first_name}</td>
@@ -154,16 +164,15 @@ function App() {
           </td>
         </tr>
       ))
-    ) : (
-      <tr>
-        <td colSpan={3}>&nbsp;</td>
-      </tr>
-    );
+    )
   };
 
+  // Function to get the contacts count
   function Count(){
     return contacts.length
   }
+
+  // The return of the App function 
   return (
     <section>      
       <div  id = "header" style={style.centerWithMargin}>
@@ -171,80 +180,80 @@ function App() {
       </div>
       <div  id = "addButtonDiv" style={style.center}>
         {!showAdd?
-      <button
-        className = "add-btn"
-        onClick={()=>{
-          setShowAdd(true)
-        }}
-      >
-        Add Contact
-        </button>
+          <button
+            className = "add-btn"
+            onClick={()=>{
+              setShowAdd(true)
+            }}
+          >
+          Add Contact
+          </button>
         :null}
       </div>
       <div style={style.centerWithMargin}>
-      { !showAdd ? 
-      <section>
-        <table className = "table" style={style.table} id = "table">
-          <thead>
-            <tr>
-              <th style={style.tableCell}>First name</th>
-              <th style={style.tableCell}>Last name</th>
-              <th style={style.tableCell}>Phone</th>
-              <th style={style.tableCell}>Email</th>
-              <th style={style.tableCell}>Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            <Display />
-          </tbody>
-        </table>
-        <h4 style = {{textAlign:"center"}} id = "counter">Total Contacts: <Count /></h4>
-        </section>
-        : null
-      }
+        {!showAdd ? 
+          <section>
+            <table className = "table" style={style.table} id = "table">
+              <thead>
+                <tr>
+                  <th style={style.tableCell}>First name</th>
+                  <th style={style.tableCell}>Last name</th>
+                  <th style={style.tableCell}>Phone</th>
+                  <th style={style.tableCell}>Email</th>
+                  <th style={style.tableCell}>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                <Display />
+              </tbody>
+            </table>
+            <h4 style = {{textAlign:"center"}} id = "counter">Total Contacts: <Count /></h4>
+          </section>
+          : null
+        }
       </div>
-<div style = {style.center}>
-  { showAdd ?  
-    <div id = "add_section" style={style.formSection}>
-          <h3>Add Contact</h3>
-    <form id = "add_form" onSubmit={handleSubmit}>
-        <label>First name:</label>
-        <br />
-        <input name="first_name" type="text" value={contactData.first_name} onChange={setFormData}/>
-      <br />
-        <label>Last name:</label>
-        <br />
-        <input name="last_name" type="text" value={contactData.last_name} onChange={setFormData}/>
-      <br />
-        <label>Phone:</label>
-        <br />
-        <input name="phone" type="text" value={contactData.phone} onChange={setFormData}/>
-      <br />
-        <label>Email:</label>
-        <br />
-        <input name="email" type="text"value={contactData.email} onChange={setFormData}/>
-      <br />
-        <label>Details:</label>
-        <br />
-        <input name="detail" type="text"value={contactData.detail} onChange={setFormData}/>
-      <br />
-      <br />
-        <input type="submit"
-          className='submit-btn'
-          value="Add Contact" 
-        />
-        <input type="button"
-          className='cancel-btn'
-          value="Cancel" 
-          onClick={() =>{setShowAdd(false)}}
-        />
-    </form>
-    <h4 style = {{color:'red'}}id = "error_add">{error}</h4>
-    </div>
-    :     
-    null
-  }
-</div>
+      <div style = {style.center}>
+        {showAdd ?  
+          <div id = "add_section" style={style.formSection}>
+            <h3>Add Contact</h3>
+            <form id = "add_form" onSubmit={handleSubmit}>
+              <label>First name:</label>
+              <br />
+              <input name="first_name" type="text" value={contactData.first_name} onChange={setFormData}/>
+              <br />
+              <label>Last name:</label>
+              <br />
+              <input name="last_name" type="text" value={contactData.last_name} onChange={setFormData}/>
+              <br />
+              <label>Phone:</label>
+              <br />
+              <input name="phone" type="text" value={contactData.phone} onChange={setFormData}/>
+              <br />
+              <label>Email:</label>
+              <br />
+              <input name="email" type="text"value={contactData.email} onChange={setFormData}/>
+              <br />
+              <label>Details:</label>
+              <br />
+              <input name="detail" type="text"value={contactData.detail} onChange={setFormData}/>
+              <br />
+              <br />
+              <input type="submit"
+                className='submit-btn'
+                value="Add Contact" 
+              />
+              <input type="button"
+                  className='cancel-btn'
+                  value="Cancel" 
+                  onClick={() =>{setShowAdd(false)}}
+              />
+            </form>
+            <h4 style = {{color:'red'}}id = "error_add">{error}</h4>
+          </div>
+          :     
+          null
+        }
+      </div>
     </section>
   );
 }
